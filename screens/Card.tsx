da9745718f9b370dcc9a281bea0ae97a08f016b6
FacundoOs <osores.facundo@gmail.com>
2021-02-22 11:29:04 +0100

    show next card and compare if it is higher or lower

--- screens/Card.tsx
+++ screens/Card.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, Button } from "react-native";
+import { View, Image, Button, Text } from "react-native";
 import axios from "axios";
 
 const Card = () => {
- const [card, setCard] = useState("")
+  const [card, setCard] = useState("");
+  const [cards_id, setCards_id] = useState(null);
+  const [value, setValue] = useState("");
+  const [actualValue, setActualValue] = useState("");
+  const [option, setOption] = useState("");
+  const [cont, setCont] = useState(0);
 
   const api = axios.create({
     baseURL: "https://deckofcardsapi.com/api/deck/",
@@ -16,21 +21,44 @@ const Card = () => {
           deck_count: 1,
         },
       });
-      // console.log(data)
       const { deck_id } = data;
       const { data: cardsResponse } = await api.get(`${deck_id}/draw/`, {
         params: {
           count: 1,
         },
       });
-      console.log(cardsResponse.cards[0].image)
-
-      setCard(cardsResponse.cards[0].image)
-
-      // return { ...cardsResponse.cards[0], deck_id };
+      console.log(cardsResponse.cards[0].image, deck_id);
+      setCard(cardsResponse.cards[0].image);
+      setCards_id(deck_id);
+      setValue(cardsResponse.cards[0].value);
     };
-    createDeckAndDraw()
+    createDeckAndDraw();
   }, []);
+
+  const buttonOnClick = async () => {
+    const { data } = await api.get(`${cards_id}/draw/`, {
+      params: {
+        count: 1,
+      },
+    });
+    console.log(data.cards[0].image);
+    setCard(data.cards[0].image);
+    setActualValue(data.cards[0].value);
+
+    if (option === "Up") {
+      if (value > actualValue) {
+        setCont(cont + 1);
+        console.log(cont);
+      }
+    }
+
+    if (option === "Down") {
+      if (value < actualValue) {
+        setCont(cont + 1);
+        console.log(cont);
+      }
+    }
+  };
 
   return (
     <View style={{ alignItems: "center" }}>
@@ -39,14 +67,25 @@ const Card = () => {
           resizeMode: "contain",
           width: 340,
           height: 540,
-          // borderRadius: 140 / 2,
           marginTop: -50,
         }}
         source={{
-          uri: card
+          uri: card,
         }}
       />
-      <Button title="Click Here" onPress={() => alert("Button Clicked!")} />
+      <Button
+        title="Up"
+        onPress={() => {
+          setOption("Up"), buttonOnClick();
+        }}
+      />
+      <Button
+        title="Down"
+        onPress={() => {
+          buttonOnClick(), setOption("Down");
+        }}
+      />
+      <Text>{cont && <Text>{cont}</Text>}</Text>
     </View>
   );
 };
